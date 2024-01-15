5cde13420b8d66628d8b1d13fff2371e87e6d469
Alex-Pablo <alexpabloanibal@gmail.com>
2024-01-14 23:12:41 -0600

    add more resources

--- src/app/resource/page.jsx
+++ src/app/resource/page.jsx
@@ -15,6 +15,20 @@ import  imgRoadmap  from "../../../public/img/roadmap.png"
 import imgSrimba from "../../../public/img/scrimba.png"
 import imgSQLBot  from "../../../public/img/sqlbolt.png"
 import  imgArchitectureNotes from "../../../public/img/architecturenotes.png"
+import imgEfset from "../../../public/img/efset.png"
+import imgRapidApi from "../../../public/img/rapidapi.png"
+import imgPythonTutor  from "../../../public/img/pythontutor.png"
+import imgUiball from "../../../public/img/uiball.png"
+import imgCoolContrast from "../../../public/img/coolcontrast.png"
+import imgCoolDesigner from "../../../public/img/colordesigner.png"
+import imgFreeFaces from "../../../public/img/freefaces.png"
+import imgMonaspace from "../../../public/img/monaspace.png"
+import imgSvgRepo from "../../../public/img/svgrepo.png"
+import imgYesicon from "../../../public/img/yesicon.png"
+import imgTabler from "../../../public/img/tabler.png"
+import imgGodly from "../../../public/img/godly.png"
+import  imgDevCommuni  from "../../../public/img/DevCommuni.png"
+
 export default function Resource() {
   const resources = [
     {
@@ -68,6 +82,83 @@ export default function Resource() {
       description:"En este artículo se explica el particionamiento de bases de datos, sus ventajas,cómo usarlo y cuándo no.",
       image: imgArchitectureNotes,
       link: 'https://architecturenotes.co/database-sharding-explained/'
+    },{
+      title: "DEV Community",
+      description:"DEV Community es una comunidad de 1.237.584 increíbles desarrolladores.",
+      image: imgDevCommuni,
+      link: 'https://dev.to/'
+    },
+    {
+      title: "Pon a prueba tu inglés",
+      description:"Evalúa de inmediato tus habilidades de comprensión oral y lectora en inglés.",
+      image: imgEfset,
+      link: 'https://www.efset.org/es/ef-set-50/'
+    },
+    {
+      title: "Colección de APIs",
+      description:"Descubra y conéctese a miles de API.",
+      image: imgRapidApi,
+      link: 'https://rapidapi.com/hub'
+    },
+    {
+      title: "Tutor + Debuggedor de codigo",
+      description:"Es una herramienta que asiste en tareas de programación en Python, Java, C, C++ y JavaScript. Incluye un depurador y un tutor IA para facilitar la comprensión y depuración.",
+      image: imgPythonTutor,
+      link: 'https://pythontutor.com/'
+    },
+    {
+      title: "Colección de cargas",
+      description:"Cargadores y spinners gratuitos y de código abierto para tu próximo proyecto, construido con HTML, CSS y SVG.",
+      image: imgUiball,
+      link: 'https://uiball.com/ldrs/'
+    },
+    {
+      title: "Contrasta tus colores para tus UI",
+      description:"Es una herramienta que facilita la selección de colores adecuados para tus interfaces, ayudándote a elegir combinaciones visuales efectivas y atractivas.",
+      image: imgCoolContrast,
+      link: 'https://coolcontrast.vercel.app/'
+    },
+    {
+      title: "Crea tus colores",
+      description:"Es una herramienta para la creacion de colores adecuados para tus interfacec..",
+      image: imgCoolDesigner,
+      link: 'https://colordesigner.io/'
+    },
+    {
+      title: "Fuentes tipograficas",
+      description:"Explora una variedad de fuentes tipográficas para descubrir nuevas opciones y enriquecer tus diseños.",
+      image: imgFreeFaces,
+      link: 'https://www.freefaces.gallery/'
+    },
+    {
+      title: "monaspace",
+      description:"Una innovadora superfamilia de fuentes para código.",
+      image: imgMonaspace,
+      link: 'https://monaspace.githubnext.com/'
+    },
+    {
+      title: "Colección de SVG",
+      description:"Busque, explore y edite los iconos o vectores gratuitos que mejor se adapten a sus proyectos utilizando una amplia variedad de bibliotecas de vectores.",
+      image: imgSvgRepo,
+      link: 'https://www.svgrepo.com/'
+    },
+    {
+      title: "Colección de Iconos",
+      description:"Conjunto de Iconos de Código Abierta y Gratis. Herramienta útil para que desarrolladores y diseñadores.",
+      image: imgYesicon,
+      link: 'https://yesicon.app/'
+    },
+    {
+      title: "Colección de Iconos",
+      description:"Iconos gratuitos y de código abierto diseñados para hacer sitios web o aplicaciónes atractivos.",
+      image: imgTabler,
+      link: 'https://tabler.io/icons'
+    },
+    {
+      title: "Inspiracion de diseño de paginas web",
+      description:"Inspiración de diseño web astronómicamente buena de todo Internet.",
+      image: imgGodly,
+      link: 'https://godly.website/'
     }
   ];
   return (
